7ba2ab7951981a6f2ccd16eb3505d03073ae6de6
paulino007 <paul@paulshoremekun.com>
2021-01-16 22:55:15 +0100

    Added url translation to project. Added text translations in templates

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 from . import views
 
 app_name = 'order'
 urlpatterns=[
-    path('create/', views.order_create, name='order_create'),
-    path('admin/order/<int:order_id>/', views.admin_order_detail, name='admin_order_detail'),
-    path('admin/order/<int:order_id>/pdf', views.admin_order_pdf, name ='admin_order_pdf')
+    path(_('create/'), views.order_create, name='order_create'),
+    path(_('admin/order/<int:order_id>/'), views.admin_order_detail, name='admin_order_detail'),
+    path(_('admin/order/<int:order_id>/pdf'), views.admin_order_pdf, name ='admin_order_pdf')
 ]
